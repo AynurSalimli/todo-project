391baf71ff9f7e3e784e2ab0888bc9aafcc424b5
AynurSalimli <aynur.selimli@ascca.edu.az>
2024-05-06 01:01:05 +0400

    adding js

--- script.js
+++ script.js
@@ -8,5 +8,32 @@ function addTask() {
     let li = document.createElement("li");
     li.innerHTML = todoBox.value;
     listTodo.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  todoBox.value = "";
+  saveData();
 }
+
+listTodo.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listTodo.innerHTML);
+}
+
+function showTask() {
+  listTodo.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
